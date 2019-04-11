131200f1364b931fca97d58f3adbc467710f0b21
AlxxOlivs <aleolidal@gmail.com>
2019-04-11 12:01:19 +0200

    Script
    He millorat el script pq pugui correr pero no se com fer lo de ajupirse

--- Treball/Scripts/Tim.gd
+++ Treball/Scripts/Tim.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 var velocitatmaxima = 100
+var velocitatpocapoc = 0
 var velocitat = Vector2()
 var gravetat = Vector2(0,9.8)
 var salt = Vector2(0,-500)
@@ -10,24 +11,36 @@ func _process(delta):
 func mou():
 	velocitat.x = 0
 	if Input.is_action_pressed("ui_right"):
+		velocitat.x = velocitatpocapoc
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_shift"):
 		velocitat.x = velocitatmaxima
-		
 	if Input.is_action_pressed("ui_left"):
+		velocitat.x = -velocitatpocapoc
+	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_shift"):
 		velocitat.x = -velocitatmaxima
-		
 	if Input.is_action_pressed("ui_up") and  is_on_floor():
 		velocitat += salt
-	
+	if Input.is_action_pressed("ui_control"):
+		pass
 	if not is_on_floor():
 		velocitat += gravetat
 	velocitat = move_and_slide(velocitat, Vector2(0,-1))
-
+	
 func anima():
 	if velocitat.x > 0:
 		$AnimationPlayer.play("Camina")
 		$Sprite.flip_h = false
+	elif velocitat.x > velocitatpocapoc:
+		$AnimationPlayer.play("Corre")
+		$Sprite.flip_h = false
 	elif velocitat.x < 0:
 		$AnimationPlayer.play("Camina")
 		$Sprite.flip_h = true
+	elif velocitat.x < velocitatpocapoc:
+		$AnimationPlayer.play("Corre")
+		$Sprite.flip_h = true
+	elif Input.is_action_pressed("ui_control"):
+		$AnimationPlayer.play("Ajupirse")
 	else:
 		$AnimationPlayer.play("Quiet")
+	
