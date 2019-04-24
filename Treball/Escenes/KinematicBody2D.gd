3c4cbe3cefdd040b1d0c66c909ad75ae0a06cda2
MiguelM1D <ermanueh03@gmail.com>
2019-04-24 12:29:04 +0200

    nivell miguel
    he millorat el nivell, he fet que la planta mori quan el enemic la toca i que l'enemic es mogui

--- Treball/Escenes/KinematicBody2D.gd
+++ Treball/Escenes/KinematicBody2D.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
-
+var velocitatpocapoc = 150
+var velocitatmaxima = 250
+var velocitat_salt = 600
+var velocitat = Vector2()
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,10 +11,22 @@ extends KinematicBody2D
 func _ready():
 	pass # Replace with function body.
 
+func _process(delta):
+	mou()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-
-func _on_Area2D_area_entered(area):
-	pass # Replace with function body.
+func mou():
+	velocitat.x = 0
+	if Input.is_action_pressed("tecla_d"):
+		velocitat.x = velocitatpocapoc
+	if Input.is_action_pressed("tecla_A"):
+		velocitat.x = -velocitatpocapoc
+	if Input.is_action_pressed("tecla_S"):
+		velocitat.y = 100
+	elif Input.is_action_pressed("tecla_w"):
+		velocitat.y = -100
+	else:
+		velocitat.y = 0
+	velocitat = move_and_slide(velocitat, Vector2(0,-1))
