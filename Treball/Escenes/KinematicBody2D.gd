bdd6e0512c1f445398b1fb2954613892384a4c6e
MiguelM1D <ermanueh03@gmail.com>
2019-04-25 12:18:29 +0200

    petits canvis
    un parell de canvis al script sense importancia

--- Treball/Escenes/KinematicBody2D.gd
+++ Treball/Escenes/KinematicBody2D.gd
@@ -1,55 +1,15 @@
 extends KinematicBody2D
-var velocitatpocapoc = 150
-var velocitatmaxima = 250
-var velocitat_salt = 600
 var velocitat = Vector2()
-var gravetat = Vector2(0,25)
-var salt = Vector2(0,-velocitat_salt)
-func _process(delta):
-	mou()
-	anima()
-	if position.y > 1000:
-		mor()
-		
 
-func mou():
-	velocitat.x = 0
+func _process(delta):
 	if Input.is_action_pressed("ui_right"):
-		velocitat.x = velocitatpocapoc
-	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_shift"):
-		velocitat.x = velocitatmaxima
-	if Input.is_action_pressed("ui_left"):
-		velocitat.x = -velocitatpocapoc
-	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_shift"):
-		velocitat.x = -velocitatmaxima
-	if Input.is_action_pressed("ui_up") and  is_on_floor():
-		velocitat += salt
-	if Input.is_action_pressed("ui_control"):
-		pass
-	if not is_on_floor():
-		velocitat += gravetat
-	velocitat = move_and_slide(velocitat, Vector2(0,-1))
-	
-func anima():
-	if velocitat.x > 0:
-		$AnimationPlayer.play("Camina")
-		$Sprite.flip_h = false
-	if not is_on_floor():
-		$AnimationPlayer.play("Salta")
-	elif velocitat.x > velocitatpocapoc:
-		$AnimationPlayer.play("Corre")
-		$Sprite.flip_h = false
-	elif velocitat.x < 0:
-		$AnimationPlayer.play("Camina")
-		$Sprite.flip_h = true
-	elif velocitat.x < velocitatpocapoc:
-		$AnimationPlayer.play("Corre")
-		$Sprite.flip_h = true
-	elif Input.is_action_pressed("ui_control"):
-		$AnimationPlayer.play("Ajupirse")
+		velocitat.x = 100
+	elif Input.is_action_pressed("ui_left"):
+		velocitat.x = -100
+	elif Input.is_action_pressed("ui_up"):
+		velocitat.y = -100
+	elif Input.is_action_pressed("ui_down"):
+		velocitat.x = 100
 	else:
-		$AnimationPlayer.play("Quiet")
-	
-func mor():
-	queue_free()
-	get_tree().reload_current_scene()
+		velocitat.x = 0
+		velocitat.y = 0
