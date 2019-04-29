f8d32dafb3563927cde15e25a39471600f7a93a2
MiguelM1D <ermanueh03@gmail.com>
2019-04-29 13:42:36 +0200

    canvi de nom
    he canviat el nom d'un script per evitar possibles problemes

--- Treball/Escenes/Enemic_Miguel.gd
+++ Treball/Escenes/Enemic_Miguel.gd
@@ -4,9 +4,12 @@ var velocitat = Vector2()
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		velocitat.x = 100
-	if Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left"):
 		velocitat.x = -100
-	if Input.is_action_pressed("ui_up"):
+	elif Input.is_action_pressed("ui_up"):
 		velocitat.y = -100
 	elif Input.is_action_pressed("ui_down"):
 		velocitat.x = 100
+	else:
+		velocitat.x = 0
+		velocitat.y = 0
