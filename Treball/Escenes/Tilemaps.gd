b71531662c32c37b0435b9c7ea3ff842a29b6029
alexfito02 <alexfito02@gmail.com>
2019-05-02 12:27:25 +0200

    Menu casi acabat, nomes falta posar-ho bé
    He fet que el menu apareixi clicant el boto i tambe es para el joc

--- Treball/Escenes/Tilemaps.gd
+++ Treball/Escenes/Tilemaps.gd
@@ -6,7 +6,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -14,4 +14,7 @@ func _ready():
 
 
 func _on_TextureButton_pressed():
-	get_tree().reload_current_scene()
+	$Menu.popup_centered()
+	get_tree().paused = true
+	
+
