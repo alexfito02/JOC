4caf8fa83909f33662d43fc763b6d5419845794c
AlxxOlivs <aleolidal@gmail.com>
2019-05-07 12:27:15 +0200

    Personatge
    Falta acabar de millorar pero ja fa un doble salt per un just pressed

--- Treball/Scripts/Tim.gd
+++ Treball/Scripts/Tim.gd
@@ -5,8 +5,7 @@ var velocitat_salt = 600
 var velocitat = Vector2()
 var gravetat = Vector2(0,25)
 var salt = Vector2(0,-velocitat_salt)
-var velocitatajupit = 50
-
+var punts = 0
 func _physics_process(delta):
 	mou()
 	
@@ -17,24 +16,28 @@ func _process(delta):
 
 func mou():
 	velocitat.x = 0
+	if velocitat.x == 0 and velocitat.y == 0:
+		punts = 1000
 	if Input.is_action_pressed("ui_right"):
 		velocitat.x = velocitatpocapoc
+		punts = 10
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_shift"):
 		velocitat.x = velocitatmaxima
+		punts = 20
 	if Input.is_action_pressed("ui_left"):
 		velocitat.x = -velocitatpocapoc
+		punts = 10
 	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_shift"):
 		velocitat.x = -velocitatmaxima
-	if Input.is_action_pressed("ui_up") and  is_on_floor():
+		punts = 20
+	if Input.is_action_just_pressed("ui_up") and  is_on_floor():
 		velocitat += salt
-	if Input.is_action_pressed("ui_control") and is_on_floor() and Input.is_action_pressed("ui_right"):
-		velocitat.x = velocitatajupit
-		$AnimationPlayer.play("Ajupirse")
-	if Input.is_action_pressed("ui_control") and is_on_floor() and Input.is_action_pressed("ui_left"):
-		velocitat.x = -velocitatajupit
-		$AnimationPlayer.play("Ajupirse")
+		punts = 0
 	if not is_on_floor():
 		velocitat += gravetat
+		if punts == 0 and Input.is_action_just_pressed("ui_up"):
+			velocitat += salt
+			punts = 5
 	velocitat = move_and_slide(velocitat, Vector2(0,-1))
 	
 	
@@ -43,17 +46,18 @@ func anima():
 		$Sprite.flip_h = true
 	elif velocitat.x > 0:
 		$Sprite.flip_h = false
-	if is_on_floor():
-		if abs(velocitat.x) == velocitatpocapoc:
-			$AnimationPlayer.play("Camina")
-		elif abs(velocitat.x) == velocitatmaxima:
-			$AnimationPlayer.play("Corre")
-		elif abs(velocitat.x) == 0:
-			$AnimationPlayer.play("Quiet")
-		else:
-			pass
-	elif abs(velocitat.y)  != 0:
+	if punts == 10:
+		$AnimationPlayer.play("Camina")
+	elif punts == 20:
+		$AnimationPlayer.play("Corre")
+	elif punts == 1000:
+		$AnimationPlayer.play("Quiet")
+	elif punts == 0:
 		$AnimationPlayer.play("Salta")
+	elif punts == 5:
+		#$AnimationPlayer.play("DobleSalt")
+		pass
+		
 func mor():
 	queue_free()
 	get_tree().reload_current_scene()
