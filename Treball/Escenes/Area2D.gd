e5d48d0289309e4d5c064b0279ba4be39c8a40b4
alexfito02 <alexfito02@gmail.com>
2019-05-16 12:22:08 +0200

    Arreglat error que no apareixia el tilemaps

--- Treball/Escenes/Area2D.gd
+++ Treball/Escenes/Area2D.gd
@@ -6,8 +6,8 @@ var vida_planta = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if vida_planta = 0:
-		
+	if vida_planta == 0:
+		pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
