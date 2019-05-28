99a395adb626d35aaa1d5508eaf143da13b88a90
MiguelM1D <ermanueh03@gmail.com>
2019-05-28 12:08:49 +0200

    menú nivells i millores nivell m
    he solucionat el problema de que els anemics no desapareixien i he afegit el fons dels nivells a la carpeta imatges alex en pixil i png

--- Treball/Escenes/NivellL.gd
+++ Treball/Escenes/NivellL.gd
@@ -21,22 +21,6 @@ func _process(delta):
 #	pass
 
 
-func _on_Area2D_area_entered(area): #Aquesta s'activa quan entren l'area de la flor
-	print("enemic dins flor")
-	vida_planta -= 25
-	if area == $Enemic:
-		$Enemic.position = Vector2(-155,-130)
-		$Enemic.moure = false
-	if area == $Enemic2:
-		$Enemic2.position = Vector2(-155,-130)
-		$Enemic2.moure = false
-	if area == $Enemic3:
-		$Enemic3.position = Vector2(-155,-130)
-		$Enemic3.moure = false
-	if area == $Enemic4:
-		$Enemic4.position = Vector2(-155,-130)
-		$Enemic4.moure = false
-
 func barra_vida_planta():
 	if vida_planta == 100:
 		$Vida_planta/TextureProgress.value = 100
@@ -49,20 +33,6 @@ func barra_vida_planta():
 	if vida_planta == 0:
 		$Vida_planta/TextureProgress.value = 0
 
-func _on_Area2D2_body_entered(body):
-	if body == $Enemic1/enemic:
-		$Enemic1.position = Vector2(-155,-130)
-		$Enemic1.moure = false
-	if body == $Enemic2/enemic2:
-		$Enemic2.position = Vector2(-155,-130)
-		$Enemic2.moure = false
-	if body == $Enemic3/enemic3:
-		$Enemic3.position = Vector2(-155,-130)
-		$Enemic3.moure = false
-	if body == $Enemic4/enemic4:
-		$Enemic4.position = Vector2(-155,-130)
-		$Enemic4.moure = false
-
 
 func _on_Enemic1_body_entered(body):
 	if body == $Protagonista:
@@ -72,15 +42,11 @@ func _on_Enemic1_body_entered(body):
 func _on_Flor_body_entered(body):
 	print("enemic dins flor")
 	vida_planta -= 25
-	if body == $Enemic1:
-		$Enemic1.position = Vector2(-155,-130)
-		$Enemic1.moure = false
-	if body == $Enemic2:
-		$Enemic2.position = Vector2(-155,-130)
-		$Enemic2.moure = false
-	if body == $Enemic3:
-		$Enemic3.position = Vector2(-155,-130)
-		$Enemic3.moure = false
-	if body == $Enemic4:
-		$Enemic4.position = Vector2(-155,-130)
-		$Enemic4.moure = false
+	if body == $Enemic1/enemic:
+		$Enemic1.queue_free()
+	if body == $Enemic2/enemic2:
+		$Enemic2.queue_free()
+	if body == $Enemic3/enemic3:
+		$Enemic3.queue_free()
+	if body == $Enemic4/enemic4:
+		$Enemic4.queue_free()
